4814f1b7d2c3322b4028e336fb036058e62ac49c
donghyeon95 <smdf2545@konkuk.ac.kr>
2020-09-02 02:32:23 +0900

    ajax-ing

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from .views import post_result
 
 app_name = 'app'
 
 urlpatterns = [
             path('', views.index, name ='index'),
             path('viewer/', views.pdf_view, name ='pdf_view'),
-            path('result/', views.result, name = 'result'),
+            # path('result/', views.result, name = 'result'),
             path('request', views.post_result, name='post_result')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
-
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'app/index.html')
@@ -10,28 +10,41 @@ def pdf_view(request):
     if request.method == 'GET':
         return render(request, 'app/viewer.html')
     elif request.method == 'POST':
-        text = [request.POST.get("search", None),request.POST.get("title", None)] 
-        return HttpResponse(text)
+        text = {
+                'title' : request.POST.get("search", None),
+                'content' :request.POST.get("title", None) 
+               }
+        return render(request, 'app/viewer.html', text)
         #return render(request, 'app/index.html')
 
-def result(request):
-#    search_key = request.GET['search_key'] 
-#    context = {'search_key':search_key} 
-#    return render(request,'result/result.html',context)
-    if request.is_ajax():
-        return render(request,'result/result.html')
-    else:
-        return render(request,'result/result.html')
-
+# def result(request):
+# # #    search_key = request.GET['search_key'] 
+# # #    context = {'search_key':search_key} 
+# # #    return render(request,'result/result.html',context)
+# #     if request.is_ajax():
+# #         return render(request,'result/result.html')
+# #     else:
+# #         return render(request,'result/result.html')
+#     pass
+@csrf_exempt
 def post_result(request):
-    keyword = request.POST.get('search_key', '')
+    if request.method == 'POST':
+        keyword = request.POST.get('search_key', None)
+        print(keyword)#it is None!!!!!
 
-    message = "임시 데이터. 적절하게 수정할 필요가 있다."
+        message = "임시 데이터. 적절하게 수정할 필요가 있다."
 
-    context = {'secret_key': message }
+        context = {'secret_key': message }
 
-    return HttpResponse(json(context), content_type="application/json")
+        keywords={'keyword': keyword}
+        # return render(request, 'app/viewer.html', keyword)
+        # return HttpResponse(keyword)
+        return JsonResponse(keywords)
+    else:
+        message = "임시 데이터. 적절하게 수정할 필요가 있다."
 
+        context = {'secret_key': message }
+        return JsonResponse(context)
 #def searchText(request):
 #   if request.method == 'GET':
 #        pass
